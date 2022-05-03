f45ff12835b63c4b0b2c6f6b54a9115ebb06e676
Kevin Rivera <kevinriveraut@gmail.com>
2022-05-03 15:33:10 -0400

    Implemented navigaton using ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'recipe-manager'
+  loadedItem = 'recipe'
+
+  onNavigate(item: string){
+    this.loadedItem = item;
+  }
 }
